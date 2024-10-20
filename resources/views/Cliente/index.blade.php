ad25cb3f5f043dada67e516cf18b4d80ba13e5dd
anacanestraro <ana.canestraro07@gmail.com>
2024-10-19 21:41:25 -0300

    botão de pesquisa mas ainda ta errado

--- resources/views/Cliente/index.blade.php
+++ resources/views/Cliente/index.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
     <title>Document</title>
+
 </head>
 <body>
-    <table>
+    <p>
+        <input oninput="w3.filterHTML('#tabelaClientes', '.cliente', this.value)" placeholder="Pesquisar Cliente">
+    </p>
+    <table id="tabelaClientes">
         <tr>
             <th>Nome</th>            
             <th>CPF</th>
@@ -17,7 +22,7 @@
         </tr>
 
         @foreach($clientes as $cliente)
-            <tr>
+            <tr class="cliente">
                 <td>{{$cliente->nome}}</td>
                 <td>{{$cliente->cpf}}</td>
                 <td>{{$cliente->telefone}}</td>
