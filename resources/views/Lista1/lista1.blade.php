a300a60434e1123249e31bc0cfa1291758fe8be3
anacanestraro <ana.canestraro07@gmail.com>
2024-11-22 17:32:39 -0300

    ajuste de listagem

--- resources/views/Lista1/lista1.blade.php
+++ resources/views/Lista1/lista1.blade.php
@@ -1,16 +1,18 @@
 <x-app-layout>
-    <x-slot name="header">
-        <p><a href="exercicio1">Exercicio 1</a></p>
-        <p><a href="exercicio2">Exercicio 2</a></p>
-        <p><a href="exercicio3">Exercicio 3</a></p>
-        <p><a href="exercicio4">Exercicio 4</a></p>
-        <p><a href="exercicio5">Exercicio 5</a></p>
-        <p><a href="exercicio6">Exercicio 6</a></p>
-        <p><a href="exercicio7">Exercicio 7</a></p>
-        <p><a href="exercicio8">Exercicio 8</a></p>
-        <p><a href="exercicio9">Exercicio 9</a></p>
-        <p><a href="exercicio10">Exercicio 10</a></p>
-        <p><a href="exercicio11">Exercicio 11</a></p>
-    </x-slot>
-    </x-app-layout>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <p><a  class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio1">Exercicio 1</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio2">Exercicio 2</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio3">Exercicio 3</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio4">Exercicio 4</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio5">Exercicio 5</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio6">Exercicio 6</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio7">Exercicio 7</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio8">Exercicio 8</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio9">Exercicio 9</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio10">Exercicio 10</a></p>
+            <p><a class="hover:text-gray-900 dark:hover:text-gray-100" href="exercicio11">Exercicio 11</a></p>
+        </div>
+    </div>
+</x-app-layout>
    
