d9a4a7f6a4da3fab27d5d47d337cf7eb82035ed5
anacanestraro <ana.canestraro07@gmail.com>
2024-11-25 12:36:22 -0300

    ajustes no atualizar

--- resources/views/Cliente/show.blade.php
+++ resources/views/Cliente/show.blade.php
@@ -1,20 +1,16 @@
-@extends('layouts.main')
-@section('title', 'Mostrar Clientes')
-@section('active', 'active')
+<x-app-layout>
+    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100"> Detalhes do cliente</h2>
 
-@section ('content')
-
-<h1>Detalhes do cliente</h1>
-<p>Nome:{{$cliente->nome}}</p>
-<p>CPF:{{$cliente->cpf}}</p>
-<p>Telefone:{{$cliente->telefone}}</p>
-<p>E-mail:{{$cliente->email}}</p>
-
-<form action="deletarCliente/{{$cliente->id}}" method="POST" onsubmit="return confirm('TEM CERTEZA?');">
-    @csrf 
-    @method('DELETE')
-    <button type="submit">Deletar</button>
-</form>
-
-<a href="editarCliente/{{$cliente->id}}" >Editar</a>
-@endsection 
+    <p>Nome:{{$cliente->nome}}</p>
+    <p>CPF:{{$cliente->cpf}}</p>
+    <p>Telefone:{{$cliente->telefone}}</p>
+    <p>E-mail:{{$cliente->email}}</p>
+    
+    <form action="deletarCliente/{{$cliente->id}}" method="POST" onsubmit="return confirm('TEM CERTEZA?');">
+        @csrf 
+        @method('DELETE')
+        <x-primary-button type="submit">Deletar</x-primary-button>
+    </form>
+    
+    <a href="editarCliente/{{$cliente->id}}" >Editar</a>
+</x-app-layout>
